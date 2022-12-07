1bab78f602ad95d83ffb2f7abab1eef66516313f
jtdolendo <jtdolendo@up.edu.ph>
2022-12-07 21:38:54 +0800

    Feat: code blocks

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -9,13 +9,36 @@ index = 0
 def program(tokens):
     global index
 
-    tokens_length = len(tokens)
     if tokens[index][1] == "Single line comment" or tokens[index][1] == "Multiple line comment starts":
         while tokens[index][1] == "Single line comment" or tokens[index][1] == "Multiple line comment starts":
             comment(tokens)
     
-    if tokens[index][1] == "Program Start Keyword":
-        pass
+    if tokens[index][1] == "Program Start Keyword" and (tokens[index-1][1] != None and tokens[index-1][1] == "New Line"):
+        print("Start of Program: " + tokens[index][0])
+        keyword1 = tokens[index][0]
+        index += 1
+
+        block_code_list = []
+        operator1 = block_code(tokens, block_code_list)
+
+        if tokens[index][1] == "Program End Keyword" and tokens[index+1][1] == "New Line":
+            print("End of Program: " + tokens[index][0])
+            keyword2 = tokens[index][0]
+
+            index += 2
+
+            while index < len(tokens):
+                if tokens[index][1] == "Single line comment" or tokens[index][1] == "Multiple line comment starts":
+                    comment(tokens)
+                else:
+                    prompt_error()
+
+            return keyword1, operator1, keyword2
+        
+        else:
+            prompt_error()
+    else:
+        prompt_error()
 
 def comment(tokens):
     global index
@@ -76,6 +99,24 @@ def linebreak(tokens):
             return True
         return False
 
+'''	CODE BLOCK:
+    <code_block>	::= <code_block2> <code_block>
+    <code_block2>	::= <print> | <declaration> | <comment> | <concat> | <input> |
+                        <exp_it> | <assignment> | <if> | <switch>
+'''
+def block_code(tokens, block_code_list):
+    operator1 = block_code_2(tokens)
+
+    if operator1 is not None: block_code_list.append(operator1)
+
+    while tokens[index][1] != "Program End Keyword":
+        block_code(tokens, block_code_list)
+
+    if len(block_code_list):
+        pass
+
+def block_code_2(tokens):
+    pass
 
 # prints an error message
 def prompt_error():
